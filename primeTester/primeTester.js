71ba66760f21ecc08eb5902de7b587a4cf609745
dantehsema@yahoo.com <dantehsema@yahoo.com>
2019-09-05 20:41:11 -0400

    added prime tester

---
+++ primeTester/primeTester.js
@@ -0,0 +1,14 @@
+
+/**
+ * A prime number is a whole number that has no other divisors other than
+ * itself and 1. Write a function that accepts a number and returns true if it's
+ * a prime number, false if it's not.
+ */
+
+var primeTester = function(n) {
+  if(typeof n !== 'number' || n < 1 || n % 1 !== 0){
+    // n isn't a number or n is less than 1 or n is not an integer
+    return false;
+  }
+  // TODO: return true if n is prime, false otherwise
+};
