bfa8b9a7a43494d1b03921591ea50dd775eef264
dantehsema@yahoo.com <dantehsema@yahoo.com>
2019-09-15 12:45:35 -0400

    added rockps

---
+++ rockpaperscissors/rockpaperscissors.js
@@ -0,0 +1,61 @@
+// 5c. Write a function that returns all possible throws a player can make 
+// in a 3 round game of rock paper scissors. 
+
+// input
+// rockPaperScissors()
+
+// output
+// [["rock", "rock", "rock"], ["rock", "rock", "paper"],
+//     ["rock", "rock", "scissors"], ["rock", "paper", "rock"],
+//              ...etc...
+//      ]
+
+
+// ************Extra Credit***************
+
+// Make your function return answers for any number of rounds
+
+// input 
+// rockPaperScissors(5)
+
+// output
+// [["rock", "rock", "rock", "paper", "scissors"], 
+// 	["rock", "rock", "paper", "rock", "scissors"],
+//     ["rock", "rock", "scissors", "paper", "scissors"], 
+//     ["rock", "paper", "rock", "paper", "scissors"],
+//              ...etc...
+//      ]
+
+
+//      r, p, s 
+	
+	
+
+
+
+//      r, r, r
+
+//      p, p, p 
+
+//      s, s, s
+
+		
+// 	r, r , p
+
+// 	r, r, s
+
+// 	r, p, p
+
+
+// 	p
+
+// 	p
+
+// 	p
+
+// 	s
+
+// 	s
+
+// 	s
+
